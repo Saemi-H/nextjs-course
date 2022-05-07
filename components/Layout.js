7ed382c67821d47120ddf32fe747017a9cf3d56d
Saemi Hong <sstyle940104@gmail.com>
2022-05-07 11:09:06 +0900

    add: Add NavBar in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 import styles from "../styles/Layout.module.css"
 
+import Nav from "./Nav"
+
 const Layout = ({children}) => {
   return (
+    <>
+    <Nav />
     <div className={styles.container}>
       <main className={styles.main}>
         {children}
       </main>
     </div>
+    </>
   )
 }
 
--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import navStyles from " ../styles/Nav.module.css"
+import navStyles from "../styles/Nav.module.css"
 
 const Nav = () => {
   return (
