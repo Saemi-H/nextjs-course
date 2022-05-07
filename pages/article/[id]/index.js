94f608085631c85974314434577d4e89dc16c800
Saemi Hong <sstyle940104@gmail.com>
2022-05-07 12:36:32 +0900

    add: Add serverside props in article index

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-const article = () => {
-  const router = useRouter()
-  const {id} =router.query
+// import { useRouter } from 'next/router'
+
+const article = ({article}) => {
+  // const router = useRouter()
+  // const {id} =router.query
 
   return (
-    <div>this is an article {id}</div>
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go back</Link>
+    </>
   )
+}
+
+export const getServerSideProps = async(context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+  const article = await res.json()
+
+  return{
+    props: {article}
+  }
 }
 
 export default article
