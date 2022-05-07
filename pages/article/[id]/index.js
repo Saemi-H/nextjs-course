5b2954382f57f65d2255c8b863cb54a25b6eaf4d
Saemi Hong <sstyle940104@gmail.com>
2022-05-07 12:16:53 +0900

    get id using use Rrouter in next

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import { useRouter } from 'next/router'
+
 const article = () => {
+  const router = useRouter()
+  const {id} =router.query
+
   return (
-    <div>this is an article</div>
+    <div>this is an article {id}</div>
   )
 }
 
