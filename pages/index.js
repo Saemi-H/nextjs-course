0869065f66f23a13cb510baebc0b9a4c0807cccc
Saemi Hong <sstyle940104@gmail.com>
2022-05-09 22:13:19 +0900

    feat: refactoring index of pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { server } from '../config';
+
 import Head from 'next/head'
 
 import ArticleList from '../components/ArticleList';
@@ -17,10 +19,19 @@ export default function Home({articles}) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return{
     props: {articles}
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+//   const articles = await res.json()
+
+//   return{
+//     props: {articles}
+//   }
+// }
