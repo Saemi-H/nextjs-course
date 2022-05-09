bd6306cec386a5af2018a98783c0a20a8889ac15
Saemi Hong <sstyle940104@gmail.com>
2022-05-09 22:17:28 +0900

    feat: refactoring index of article

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { server } from "../../../config";
+
 import Link from 'next/link'
 
 // import { useRouter } from 'next/router'
@@ -28,7 +30,7 @@ const article = ({article}) => {
 // }
 
 export const getStaticProps = async(context) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+  const res = await fetch(`${server}/api/articles/${context.params.id}`)
   const article = await res.json()
 
   return{
@@ -37,7 +39,7 @@ export const getStaticProps = async(context) => {
 }
 
 export const getStaticPaths = async() => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   const ids = articles.map(article => article.id)
